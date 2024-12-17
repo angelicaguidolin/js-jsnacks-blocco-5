88b06a63e4d3436248d5761582130cfe81aff569
angelica <guidolinangelica66@gmail.com>
2024-12-17 12:29:35 +0100

    finisc snack four

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const namePost = posts.map((elm)=>{
+  return elm.author
+})
+console.log(namePost)
